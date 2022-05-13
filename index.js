08725f5fd42591bba16c6997f00d2f7d5b0b328e
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-14 01:53:41 +0600

    create post method for booking

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-//1
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-//3
 const cors = require('cors')
 require('dotenv').config()
 
-//4
+
 app.use(cors())
 app.use(express.json())
 
-//5 mongo 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lyeo0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-//6
+
 async function run() {
     try {
         //7
         await client.connect();
-        console.log('connected');
         const appointmentCollection = client.db("doctors_portal").collection("appointment");
+        const bookingCollection = client.db("doctors_portal").collection("bookings");
 
         //8 load data 
         app.get('/appointment' , async(req,res) => {
@@ -32,6 +29,13 @@ async function run() {
             const cursor = appointmentCollection.find(query)
             const appointments = await cursor.toArray() 
             res.send(appointments)
+        })
+
+        // post 
+        app.post('/booking' ,async(req,res) => {
+            const booking = req.body 
+            const result = await bookingCollection.insertOne(booking)
+            res.send(result)
         })
 
 
@@ -47,7 +51,7 @@ run().catch(console.dir);
 
 
 
-//2
+
 app.get('/', (req, res) => {
     res.send('Doctors server is running ')
 })
